b6e1d699f5c39865abb81782938972f2dba62639
sgarmendia <sgarmendia@gmail.com>
2023-04-20 01:08:45 +0200

    new text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye react app docker
+          This react app is deployed in a docker container
         </p>
         <a
           className="App-link"
